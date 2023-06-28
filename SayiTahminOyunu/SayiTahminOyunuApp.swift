845eda1865bbb787c973de323b8b6debb59557ed
hhakangull <hakanngul@icloud.com>
2023-06-29 00:36:03 +0100

    Initial Commit

---
+++ SayiTahminOyunu/SayiTahminOyunuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SayiTahminOyunuApp.swift
+//  SayiTahminOyunu
+//
+//  Created by Hakan Gül on 29/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct SayiTahminOyunuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
